5331c296813fba1a51565b3f06264a11fcba54cf
CarloVerdi01 <148077625+CarloVerdi01@users.noreply.github.com>
2024-06-19 21:38:28 +0200

    Update settings.py

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,9 +77,10 @@ WSGI_APPLICATION = "ecommerce_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+   'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=BASE_DIR,
+        conn_max_age=600
     }
 }
 
